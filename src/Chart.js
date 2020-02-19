de07f21e127849caf7540471bbbece9a49b7cfe4
sujunglee <sujunglee2020@u.northwestern.edu>
2020-02-19 17:38:53 -0600

    Create split pie chart with hard coded data

--- src/Chart.js
+++ src/Chart.js
@@ -3,10 +3,45 @@ import { Text } from 'react-native-svg'
 import { PieChart } from 'react-native-svg-charts'
 
 
+const data = [
+    {
+        key: 1,
+        amount: 50,
+        svg: { fill: '#600080' },
+    },
+    {
+        key: 2,
+        amount: 50,
+        svg: { fill: '#9900cc' }
+    },
+    {
+        key: 3,
+        amount: 40,
+        svg: { fill: '#c61aff' }
+    },
+    {
+        key: 4,
+        amount: 95,
+        svg: { fill: '#d966ff' }
+    },
+    {
+        key: 5,
+        amount: 35,
+        svg: { fill: '#ecb3ff' }
+    }
+]
+
 const Chart = () => {
   return (
     <div>
-      <h1>Results</h1>
+        <PieChart
+            style={{ height: 200 }}
+            valueAccessor={({ item }) => item.amount}
+            data={data}
+            spacing={0}
+            outerRadius={'95%'}
+        >
+        </PieChart>
     </div>)
 }
 
