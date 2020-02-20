b7fec40dc6a0139fdf6c23d2b5a151066cfd9b64
sujunglee <sujunglee2020@u.northwestern.edu>
2020-02-19 18:22:29 -0600

    Add ability to pass in data as props to Chart component

--- src/Chart.js
+++ src/Chart.js
@@ -2,36 +2,9 @@ import React from 'react';
 import { Text } from 'react-native-svg'
 import { PieChart } from 'react-native-svg-charts'
 
+const Chart = ({data}) => {
+  console.log("CHART DATA: ", data);
 
-const data = [
-    {
-        key: 1,
-        amount: 50,
-        svg: { fill: '#600080' },
-    },
-    {
-        key: 2,
-        amount: 50,
-        svg: { fill: '#9900cc' }
-    },
-    {
-        key: 3,
-        amount: 40,
-        svg: { fill: '#c61aff' }
-    },
-    {
-        key: 4,
-        amount: 95,
-        svg: { fill: '#d966ff' }
-    },
-    {
-        key: 5,
-        amount: 35,
-        svg: { fill: '#ecb3ff' }
-    }
-]
-
-const Chart = () => {
   return (
     <div>
         <PieChart
--- src/results.js
+++ src/results.js
@@ -2,10 +2,40 @@ import React from 'react';
 import Chart from "./Chart";
 
 
+const data1 = [
+    {
+        key: 1,
+        amount: 50,
+        svg: { fill: '#600080' },
+    },
+    {
+        key: 2,
+        amount: 50,
+        svg: { fill: '#9900cc' }
+    },
+    {
+        key: 3,
+        amount: 40,
+        svg: { fill: '#c61aff' }
+    },
+    {
+        key: 4,
+        amount: 95,
+        svg: { fill: '#d966ff' }
+    },
+    {
+        key: 5,
+        amount: 35,
+        svg: { fill: '#ecb3ff' }
+    }
+]
+
+
+
 const Results = () => {
   return (
     <div>
-      <Chart />
+      <Chart data={data1}/>
     </div>)
 }
 
