9d2b15647891d681284cf68521e0fc8cd1fc0006
sujunglee <sujunglee2020@u.northwestern.edu>
2020-02-20 20:31:35 -0600

    Add number labels for each section on circular chart

--- src/components/Chart.js
+++ src/components/Chart.js
@@ -1,19 +1,44 @@
 import React from "react"
-import { View, Text } from "react-native"
+import { View } from "react-native"
+import { Text } from 'react-native-svg' // Different from react-native Text!
 import { PieChart } from "react-native-svg-charts"
+
+const Labels = ({ slices, height, width }) => {
+  return slices.map((slice, index) => {
+      const { labelCentroid, pieCentroid, data } = slice;
+      return (
+          <Text
+              key={index}
+              x={pieCentroid[ 0 ]}
+              y={pieCentroid[ 1 ]}
+              fill={'white'}
+              textAnchor={'middle'}
+              alignmentBaseline={'middle'}
+              fontSize={24}
+              stroke={'black'}
+              strokeWidth={0.2}
+          >
+              {data.amount}
+          </Text>
+      )
+  })
+}
 
 const Chart = ({ data }) => {
   console.log("CHART DATA: ", data)
 
   return (
     <View>
-      <PieChart
-        style={{ height: 140, width: 140 }}
+    <PieChart
+        style={{ height: 200 }}
         valueAccessor={({ item }) => item.amount}
         data={data}
         spacing={0}
-        outerRadius={"100%"}
-      />
+        outerRadius={'95%'}
+    >
+        <Labels />
+      </PieChart>
+
     </View>
   )
 }
