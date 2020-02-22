665209d35b4979eeebd56fd1ccbf74dde28edd5b
Jidé Anene <dominicanene@gmail.com>
2020-02-22 02:28:26 -0600

    Added countdown to app. - Hard coded a 30second run off

--- src/components/countdown/CountDownStyle.js
+++ src/components/countdown/CountDownStyle.js
@@ -3,8 +3,6 @@ import {StyleSheet} from 'react-native';
 //Loading custom fonts --> https://dev.to/edo_begagic/add-custom-fonts-to-react-native-expo-app-54d
 export const styles = StyleSheet.create({
     container: {
-        paddingTop: 700,
-        paddingLeft: 10,
         alignItems: 'center',
         justifyContent: 'center',
     },
