3ba986ea81175eba3b8216319c6aa194ad385a33
Julia <juliabrain2020@u.northwestern.edu>
2020-02-22 23:48:13 -0600

    added voting
    updates db as you click button

--- src/components/Vote/VoteScreen.js
+++ src/components/Vote/VoteScreen.js
@@ -6,19 +6,25 @@ import getRoomData from "../../db/getRoomData"
 import { StyledText } from "../StyledText"
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import updateVotes from "../../db/updateVotes"
 
-const VoteScreen = ({navigation}) => {
+const enterVote = ({ navigation, roomID, selection, userID, badge }) => {
+    const results = updateVotes({ roomID: roomID, selection: selection, userID: userID, badge: badge })
+    //navigation.navigate('Results', { name: 'Jane' })
+}
+
+const VoteScreen = ({ navigation, roomID, userID, badge }) => {
     const [roomData, setRoomData] = useState(null);
 
     useEffect(() => {
         const getRoom = async () => {
-            const data = await getRoomData({ roomID: "room1" })
+            const data = await getRoomData({ roomID: roomID })
             setRoomData(data)
         }
         getRoom()
     }, [])
 
-    const Stack = createStackNavigator();
+    //const Stack = createStackNavigator();
 
     return roomData ? (
         <SafeAreaView style={{ flex: 1 }}>
@@ -44,12 +50,24 @@ const VoteScreen = ({navigation}) => {
                     <View style={{ alignItems: "center", flex: 1 }}>
                         <Image source={{ uri: roomData.pictureA }} style={{ width: 150, height: 200 }} />
                         <StyledText type="bold" size={20} style={{ paddingTop: 10 }}>Option A</StyledText>
-                        <VoteButton content="A" onPress={() => navigation.navigate('Results', {name: 'Jane'})} />
+                        <VoteButton content="A" onPress={enterVote({
+                            navigation: navigation,
+                            selection: 'optionA',
+                            roomID: roomID,
+                            userID: userID,
+                            badge: badge
+                        })} />
                     </View>
                     <View style={{ alignItems: "center", flex: 1 }}>
                         <Image source={{ uri: roomData.pictureB }} style={{ width: 150, height: 200 }} />
                         <StyledText type="bold" size={20} style={{ paddingTop: 10 }}>Option B</StyledText>
-                        <VoteButton content="B" onPress={() => navigation.navigate('Results', {name: 'Jane'})} />
+                        <VoteButton content="B" onPress={enterVote({
+                            navigation: navigation,
+                            selection: 'optionB',
+                            roomID: roomID,
+                            userID: userID,
+                            badge: badge
+                        })} />
                     </View>
                 </View>
                 <SkipButton />
