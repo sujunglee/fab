f5489f15b7adc35499d299c6178475452965f7d9
Zack <szaslan@gmail.com>
2020-02-24 23:07:21 -0600

    vote scren changes now

--- src/components/Vote/VoteScreen.js
+++ src/components/Vote/VoteScreen.js
@@ -1,80 +1,108 @@
-import React, { useEffect, useState } from "react"
+import React, { useState } from "react"
 import { View, Text, Image } from "react-native"
 import { VoteButton, SkipButton } from "./VoteButton"
-import { SafeAreaView } from "react-native-safe-area-context"
-import getRoomData from "../../db/getRoomData"
+import { SafeAreaView, useSafeArea } from "react-native-safe-area-context"
 import { StyledText } from "../StyledText"
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import { useNavigation } from "@react-navigation/native"
 import updateVotes from "../../db/updateVotes"
 
-const enterVote = ({ navigation, roomID, selection, userID, badge }) => {
-    const results = updateVotes({ roomID: roomID, selection: selection, userID: userID, badge: badge })
-    //navigation.navigate('Results', { name: 'Jane' })
-}
+const VoteScreen = ({ roomData, userID, badge, handleNextRoom }) => {
+  const navigation = useNavigation()
+  const [results, setResults] = useState(null)
+  const handlePress = async selection => {
+    const roomID = roomData.id
+    const results = await updateVotes({
+      roomID: roomID,
+      selection: selection,
+      userID: userID,
+      badge: badge
+    })
+    setResults(results)
 
-const VoteScreen = ({ navigation, roomID, userID, badge }) => {
-    const [roomData, setRoomData] = useState(null);
+    /*
+        Janky settimeout to show results for 1.5 seconds
+    */
 
-    useEffect(() => {
-        const getRoom = async () => {
-            const data = await getRoomData({ roomID: roomID })
-            setRoomData(data)
-        }
-        getRoom()
-    }, [])
+    const delay = 1500
+    setTimeout(() => {
+      handleNextRoom()
+      setResults(null)
+    }, delay)
+  }
+  //const Stack = createStackNavigator();
 
-    //const Stack = createStackNavigator();
-
-    return roomData ? (
-        <SafeAreaView style={{ flex: 1 }}>
+  return roomData ? (
+    <SafeAreaView style={{ flex: 1 }}>
+      <View
+        style={{
+          alignItems: "center",
+          flexDirection: "column",
+          width: "100%"
+        }}
+      >
+        <View style={{ padding: 25, maxHeight: 150 }}>
+          <StyledText type="bold" size={23}>
+            {roomData.room.meta_data.title}
+          </StyledText>
+        </View>
+        <View style={{ width: "100%" }}>
+          <View
+            style={{
+              flexDirection: "row"
+              //   width: "100%",
+              //   justifyContent: "space-between"
+              //   padding: 48
+            }}
+          >
             <View
-                style={{
-                    alignItems: "center",
-                    height: "100%"
-                }}
+              style={{ alignItems: "center", flex: 1, marginHorizontal: 4 }}
             >
-                <View style={{ padding: 25, height: 150 }}>
-                    <StyledText type="bold" size={23}>
-                        {roomData.title}
-                    </StyledText>
-                </View>
-                <View
-                    style={{
-                        flexDirection: "row",
-                        width: "100%",
-                        justifyContent: "space-between",
-                        padding: 48
-                    }}
-                >
-                    <View style={{ alignItems: "center", flex: 1 }}>
-                        <Image source={{ uri: roomData.pictureA }} style={{ width: 150, height: 200 }} />
-                        <StyledText type="bold" size={20} style={{ paddingTop: 10 }}>Option A</StyledText>
-                        <VoteButton content="A" onPress={() => enterVote({
-                            navigation: navigation,
-                            selection: 'optionA',
-                            roomID: roomID,
-                            userID: userID,
-                            badge: badge
-                        })} />
-                    </View>
-                    <View style={{ alignItems: "center", flex: 1 }}>
-                        <Image source={{ uri: roomData.pictureB }} style={{ width: 150, height: 200 }} />
-                        <StyledText type="bold" size={20} style={{ paddingTop: 10 }}>Option B</StyledText>
-                        <VoteButton content="B" onPress={() => enterVote({
-                            navigation: navigation,
-                            selection: 'optionB',
-                            roomID: roomID,
-                            userID: userID,
-                            badge: badge
-                        })} />
-                    </View>
-                </View>
-                <SkipButton />
+              <Image
+                source={{ uri: roomData.room.optionA.picture }}
+                style={{ width: 200, height: 300 }}
+                resizeMode="contain"
+              />
+              <StyledText type="bold" size={20} style={{ paddingTop: 10 }}>
+                Option A
+              </StyledText>
             </View>
-        </SafeAreaView>
-    ) : (
-            <Text>Loading...</Text>
-        )
+            <View
+              style={{ alignItems: "center", flex: 1, marginHorizontal: 4 }}
+            >
+              <Image
+                source={{ uri: roomData.room.optionB.picture }}
+                style={{ width: 200, height: 300 }}
+                resizeMode="contain"
+              />
+              <StyledText type="bold" size={20} style={{ paddingTop: 10 }}>
+                Option B
+              </StyledText>
+            </View>
+          </View>
+          {results ? (
+            <StyledText type="bold">Results go here </StyledText>
+          ) : (
+            <View style={{ alignItems: "center", flexDirection: "column" }}>
+              <View
+                style={{
+                  display: "flex",
+                  flexDirection: "row",
+                  width: "100%",
+                  marginTop: 8,
+                  justifyContent: "space-evenly"
+                }}
+              >
+                <VoteButton content="A" onPress={() => handlePress("A")} />
+                <VoteButton content="B" onPress={() => handlePress("B")} />
+              </View>
+              <SkipButton onPress={handleNextRoom} style={{ marginTop: 8 }} />
+            </View>
+          )}
+        </View>
+      </View>
+    </SafeAreaView>
+  ) : (
+    <Text>Loading...</Text>
+  )
 }
 export default VoteScreen
