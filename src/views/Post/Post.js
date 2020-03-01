b79a69bdc6341d37bdc428c83f1891f3cb7669c4
sujunglee <sujunglee2020@u.northwestern.edu>
2020-03-01 16:31:34 -0600

    Fix bug where image is not displayed even though you pass correct uri

--- src/views/Post/Post.js
+++ src/views/Post/Post.js
@@ -1,9 +1,91 @@
-import React from "react"
+import React,{useEffect,useState} from "react"
+import { View, Text, Button, Image, StyleSheet} from "react-native"
+import { SafeAreaView } from "react-native-safe-area-context"
 import {CameraApp} from "../../components/CameraApp";
 import { createStackNavigator,CardStyleInterpolators } from '@react-navigation/stack';
-import PostPage from './PostPage';
-
+import { useNavigation } from "@react-navigation/native"
 const Stack = createStackNavigator();
+
+
+const styles = StyleSheet.create({
+  container: {
+    width: "100%",
+    backgroundColor: "white",
+    marginBottom: 2,
+    // borderColor: "grey",
+    // borderWidth: 1,
+    padding: 16
+  },
+  image: {
+    aspectRatio: 2 / 3,
+    height: 200,
+    width: 200
+  }
+})
+
+// The actual post page
+const PostPage = ({navigation, route}) =>{
+
+    const [outfitA_uri, setOutfitA_uri] = useState(null);
+    const [outfitB_uri, setOutfitB_uri] = useState(null);
+    // const navigation = useNavigation();
+
+
+    useEffect(()=>{
+       // Set the tabbar to visible just in case
+        navigation.setOptions({
+            tabBarVisible: true
+        });
+
+        // get the uri for optionA or optionB from the camera
+        if (route.params?.uri) {
+        // if (route.params !== undefined){
+            if (route.params.outfitOption==='A'){
+                console.log(route.params.uri);
+                setOutfitA_uri(route.params.uri);
+                console.log("****THE URI FOR OUTFIT A: ", route.params.uri)
+            }
+            if (route.params.outfitOption==='B'){
+                 console.log(route.params.uri);
+                setOutfitB_uri(route.params.uri);
+            }
+        }
+
+    },[route]);
+
+    console.log("ALWAYS PRINT: ", route.params);
+
+    if (route.params) {
+      console.log("The current URI: ", route.params);
+    }
+
+
+    return(
+        <SafeAreaView>
+            <Text> My post page</Text>
+            {route.params ===undefined?
+              <View>
+                <Button
+                    title="Go to Camera for A"
+                    onPress={() => navigation.navigate('CameraApp',
+                              {outfitOption: 'A', uri:""})}
+                    />
+
+                <Button
+                title="Go to Camera for B"
+                onPress={() => navigation.navigate('CameraApp', {outfitOption: 'B'})}
+                />
+              </View>
+              :
+          <View>
+            <Image source={{uri: route.params.uri}} style={styles.image}/>
+          </View>}
+
+        </SafeAreaView>
+    )
+};
+
+
 const Post = () => {
   return (
         <Stack.Navigator gestureDirection="vertical"
@@ -21,6 +103,7 @@ const Post = () => {
         </Stack.Navigator>
   )
 };
+
 
 
 export default Post
