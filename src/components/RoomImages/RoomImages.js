d0e2f9276ca3c900ce683615b8c9c8b98849530c
Zack <szaslan@gmail.com>
2020-03-03 19:14:24 -0600

    [VoteScreen] implement UI changes on vote screen, closes #100

--- src/components/RoomImages/RoomImages.js
+++ src/components/RoomImages/RoomImages.js
@@ -1,149 +1,134 @@
-import React, {useState, useEffect} from "react"
-import {View, Image, Modal, ImageBackground, StyleSheet, TouchableWithoutFeedback} from "react-native"
-import PropTypes from 'prop-types';
-import ImageViewer from "react-native-image-zoom-viewer";
-import {StyledText} from "../StyledText";
-import {colors, normalize, sizes} from "../../constants/styles";
+import React, { useState, useEffect, forwardRef } from "react"
+import {
+  View,
+  Image,
+  Modal,
+  StyleSheet,
+  TouchableWithoutFeedback
+} from "react-native"
+import PropTypes from "prop-types"
+import ImageViewer from "react-native-image-zoom-viewer"
 
-let x = 'https://firebasestorage.googleapis.com/v0/b/fabapp-a1ea0.appspot.com/o/my-image.jpg?alt=media&token=995d6347-0435-41ac-96e1-91106786ab2c'
-
-const YourVote = () => (
-    <View
-        style={{
-            backgroundColor: colors.secondary.main,
-            width: "100%",
-            alignItems: "center",
-            paddingVertical: 8,
-            position: "relative",
-            top: -60
-        }}
-    >
-
-        <StyledText size={sizes.medium.fontSize} type={'regular'}
-                    style={{position: "relative", color: colors.general.white}}>
-            YOUR VOTE
-        </StyledText>
-    </View>
-);
+let x =
+  "https://firebasestorage.googleapis.com/v0/b/fabapp-a1ea0.appspot.com/o/my-image.jpg?alt=media&token=995d6347-0435-41ac-96e1-91106786ab2c"
 
 //const testPictures = 'https://i.imgur.com/VakBHis.jpg';
 
-const RoomImages = ({roomData, selectedOption, imageLoadCallback}) => {
-    console.log(selectedOption);
-    console.log(roomData);
+const RoomImages = props => {
+  const {
+    roomData,
+    selectedOption,
+    imageLoadCallback,
+    setImageViewport
+  } = props
+  console.log(props)
+  console.log(selectedOption)
+  console.log(roomData)
 
-    //roomData.room.optionA.picture = testPictures;
-    //roomData.room.optionB.picture = testPictures;
+  //roomData.room.optionA.picture = testPictures;
+  //roomData.room.optionB.picture = testPictures;
 
-    const [isImageOpen, setIsImageOpen] = useState(false);
-    const [areImagesLoaded, setAreImagesLoaded] = useState({
-        A: false,
-        B: false
-    });
+  const [isImageOpen, setIsImageOpen] = useState(false)
+  const [areImagesLoaded, setAreImagesLoaded] = useState({
+    A: false,
+    B: false
+  })
 
-    useEffect(() => {
-            if (areImagesLoaded.A === true && areImagesLoaded.B === true) {
-                imageLoadCallback();
-            }
-        },
-        [areImagesLoaded]
-    );
+  useEffect(() => {
+    if (areImagesLoaded.A === true && areImagesLoaded.B === true) {
+      imageLoadCallback()
+    }
+  }, [areImagesLoaded])
 
-    return (
-        <View style={styles.container}>
+  return (
+    <View
+      style={styles.container}
+      onLayout={event => {
+        const { x, y, width, height } = event.nativeEvent.layout
+        setImageViewport({ x, y, width, height })
+      }}
+    >
+      {/*Modal for image zoom-in + expansion*/}
+      {isImageOpen.state && (
+        <Modal visible={isImageOpen.state}>
+          <ImageViewer
+            enableImageZoom
+            enableSwipeDown
+            onSwipeDown={() => setIsImageOpen({ state: false })}
+            imageUrls={[
+              {
+                url: isImageOpen.url
+              }
+            ]}
+          />
+        </Modal>
+      )}
 
+      <View style={styles.photo_option}>
+        <TouchableWithoutFeedback
+          onPress={() =>
+            setIsImageOpen({
+              state: true,
+              url: roomData.room.optionA.picture
+            })
+          }
+        >
+          <Image
+            source={{ uri: roomData.room.optionA.picture }}
+            style={styles.image}
+            onLoad={() => setAreImagesLoaded({ ...areImagesLoaded, A: true })}
+          />
+        </TouchableWithoutFeedback>
+      </View>
 
-            {/*Modal for image zoom-in + expansion*/}
-            {isImageOpen.state &&
-            <Modal visible={isImageOpen.state}>
-                <ImageViewer
-                    enableImageZoom
-                    enableSwipeDown
-                    onSwipeDown={() => setIsImageOpen({state: false})}
-                    imageUrls={[
-                        {
-                            url: isImageOpen.url
-                        }
-                    ]}
-                />
-            </Modal>}
-
-            <View style={styles.photo_option}>
-                <TouchableWithoutFeedback
-                    onPress={() =>
-                        setIsImageOpen({
-                            state: true,
-                            url: roomData.room.optionA.picture
-                        })
-                    }>
-
-                    <Image
-                        source={{uri: roomData.room.optionA.picture}}
-                        style={styles.image}
-                        onLoad={() =>
-                            setAreImagesLoaded({...areImagesLoaded, A: true})
-                        }
-                    />
-                </TouchableWithoutFeedback>
-
-                {selectedOption === "optionA" && <YourVote />}
-            </View>
-
-            <View style={styles.photo_option}>
-                <TouchableWithoutFeedback
-                    onPress={() =>
-                        setIsImageOpen({
-                            state: true,
-                            url: roomData.room.optionB.picture
-                        })
-                    }>
-
-                    <Image
-                        source={{uri: roomData.room.optionB.picture}}
-                        style={styles.image}
-                        onLoad={() =>
-                            setAreImagesLoaded({...areImagesLoaded, B: true})
-                        }
-                    />
-                </TouchableWithoutFeedback>
-
-                 {selectedOption === "optionB" && <YourVote />}
-            </View>
-        </View>
-    )
-};
-
+      <View style={styles.photo_option}>
+        <TouchableWithoutFeedback
+          onPress={() =>
+            setIsImageOpen({
+              state: true,
+              url: roomData.room.optionB.picture
+            })
+          }
+        >
+          <Image
+            source={{ uri: roomData.room.optionB.picture }}
+            style={styles.image}
+            onLoad={() => setAreImagesLoaded({ ...areImagesLoaded, B: true })}
+          />
+        </TouchableWithoutFeedback>
+      </View>
+    </View>
+  )
+}
 
 const styles = StyleSheet.create({
-    photo_option: {
-        width: '48.5%',
-        height: '100%',
-        borderColor: '#A9A9A9',
-        borderWidth: 0.5,
-        backgroundColor: '#E8E8E8',
-        borderRadius: 2,
-        alignItems: 'center',
-        justifyContent: 'center'
-    },
-    image: {
-    height: '100%',
-    width: '100%',
-    resizeMode: 'cover',
-
-    },
-    container: {
-        width: '100%',
-        height: '100%',
-        justifyContent: 'space-around',
-        flexDirection: 'row'
-    },
-});
-
+  photo_option: {
+    width: "48.5%",
+    height: "100%",
+    borderColor: "#A9A9A9",
+    borderWidth: 0.5,
+    backgroundColor: "#E8E8E8",
+    borderRadius: 2,
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  image: {
+    height: "100%",
+    width: "100%",
+    resizeMode: "cover"
+  },
+  container: {
+    width: "100%",
+    height: "100%",
+    justifyContent: "space-around",
+    flexDirection: "row"
+  }
+})
 
 RoomImages.propTypes = {
-    roomData: PropTypes.object.isRequired,
-    selectedOption: PropTypes.string,
-    imageLoadCallback: PropTypes.func
-};
+  roomData: PropTypes.object.isRequired,
+  selectedOption: PropTypes.string,
+  imageLoadCallback: PropTypes.func
+}
 
-export default RoomImages;
+export default RoomImages
--- src/components/Vote/VoteScreen.js
+++ src/components/Vote/VoteScreen.js
@@ -1,30 +1,33 @@
-import React, { useState } from "react"
-import { View, Text, Dimensions, StyleSheet} from "react-native"
+import React, { useState, useRef, useEffect, useLayoutEffect } from "react"
+import { View, Text, Dimensions, StyleSheet } from "react-native"
 import { VoteButton, SkipButton } from "./VoteButton"
 import { SafeAreaView } from "react-native-safe-area-context"
 import { StyledText } from "../StyledText"
 import updateVotes from "../../db/updateVotes"
-import {colors, normalize, sizes} from "../../constants/styles"
-import CountDown from "../countdown/CountDown";
-import moment from "moment";
-import {VotingChart} from "../VotingChart";
-import {RoomImages} from "../RoomImages";
-import {RoomTitle} from "../RoomTitle";
-import Loader from "../FancyLoader/FancyLoader";
+import { colors, normalize, sizes } from "../../constants/styles"
+import CountDown from "../countdown/CountDown"
+import moment from "moment"
+import { VotingChart } from "../VotingChart"
+import { RoomImages } from "../RoomImages"
+import { RoomTitle } from "../RoomTitle"
+import Loader from "../FancyLoader/FancyLoader"
 
 function getRndInteger(min, max) {
-  return Math.floor(Math.random() * (max - min) ) + min;
+  return Math.floor(Math.random() * (max - min)) + min
 }
 
-
 const VoteScreen = ({ roomData, userID, badge, handleNextRoom }) => {
+  const [voteState, setVoteState] = useState({})
+  const deviceWidth = Dimensions.get("window").width
+  const [areImagesLoaded, setAreImagesLoaded] = useState(false)
+  const [imageViewport, setImageViewport] = useState({})
 
-  const [voteState, setVoteState] = useState({});
-  const deviceWidth = Dimensions.get('window').width;
-  const [areImagesLoaded, setAreImagesLoaded] = useState(false);
+  useEffect(() => {
+    console.log(imageViewport)
+  }, [imageViewport])
 
   const handlePress = async selection => {
-    const roomID = roomData.id;
+    const roomID = roomData.id
     const voteResults = await updateVotes({
       roomID: roomID,
       selection: selection,
@@ -37,7 +40,6 @@ const VoteScreen = ({ roomData, userID, badge, handleNextRoom }) => {
       voteResults
     })
 
-
     /*
         Janky settimeout to show results for 1.5 seconds
     */
@@ -46,72 +48,108 @@ const VoteScreen = ({ roomData, userID, badge, handleNextRoom }) => {
     setTimeout(() => {
       handleNextRoom()
       setVoteState({})
-      setAreImagesLoaded(false);
+      setAreImagesLoaded(false)
     }, delay)
   }
 
   const handleSkip = () => {
-  setAreImagesLoaded(false);
+    setAreImagesLoaded(false)
     handleNextRoom()
   }
 
-  const imageLoadCallback = () =>{
-    setAreImagesLoaded(true);
-  };
+  const imageLoadCallback = () => {
+    setAreImagesLoaded(true)
+  }
 
   return roomData ? (
     <SafeAreaView>
-
-
       <View style={styles.container}>
+        {/*
+        black screen overlay and vote results over images
+        */}
 
         {/*Room Title*/}
         <View style={styles.title_container}>
-          <RoomTitle title={roomData.room.meta_data.title}/>
+          <RoomTitle title={roomData.room.meta_data.title} />
         </View>
         {/*Room Images*/}
+
         <View style={styles.photo_container}>
           <RoomImages
-              roomData={roomData}
-              selectedOption={voteState.selectedOption}
-              imageLoadCallback={imageLoadCallback}/>
+            roomData={roomData}
+            selectedOption={voteState.selectedOption}
+            imageLoadCallback={imageLoadCallback}
+            setImageViewport={setImageViewport}
+          />
         </View>
 
+        {voteState.voteResults && (
+          <>
+            <View
+              style={{
+                ...StyleSheet.absoluteFillObject,
+                backgroundColor: "rgba(0, 0, 0, 0.7)"
+              }}
+            />
+            {voteState.voteResults.scoreA === 0 &&
+              voteState.voteResults.scoreB === 0 && (
+                <View
+                  style={{
+                    width: "100%",
+                    flexDirection: "column",
+                    alignItems: "center",
+                    bottom: imageViewport.height / 2 + 10
+                  }}
+                >
+                  <StyledText type="bold" style={{ color: "white" }}>
+                    You've made the first vote.
+                  </StyledText>
+                  <StyledText style={{ color: "white" }}>
+                    So far you are 100%!
+                  </StyledText>
+                </View>
+              )}
+            <YourVote selectedOption={voteState.selectedOption} />
+          </>
+        )}
 
-          <View style={{ width: "100%", height:'48%' }}>
-          {voteState.voteResults ?
-              (<VotingChart  voteResults={voteState.voteResults}/>)
-              : areImagesLoaded ? (
+        <View style={{ width: "100%", height: "48%" }}>
+          {voteState.voteResults ? (
+            <VotingChart voteResults={voteState.voteResults} />
+          ) : areImagesLoaded ? (
+            <View style={styles.buttons_and_timer_container}>
+              <View style={styles.buttons_container}>
+                <View style={styles.options_container_text}>
+                  <StyledText size={normalize(18)} style={styles.option_text}>
+                    Option A
+                  </StyledText>
+                  <VoteButton
+                    content="A"
+                    onPress={() => handlePress("optionA")}
+                  />
+                </View>
 
-                  <View style={styles.buttons_and_timer_container}>
+                <View style={styles.options_container_text}>
+                  <StyledText size={normalize(18)} style={styles.option_text}>
+                    Option B
+                  </StyledText>
+                  <VoteButton
+                    content="B"
+                    onPress={() => handlePress("optionB")}
+                  />
+                </View>
+              </View>
 
-                    <View style={styles.buttons_container}>
-
-                      <View style={styles.options_container_text}>
-                        <StyledText size={normalize(18)} style={styles.option_text}>
-                          Option A
-                        </StyledText>
-                        <VoteButton content="A" onPress={() => handlePress("optionA")} />
-                      </View>
-
-                      <View style={styles.options_container_text}>
-                        <StyledText  size={normalize(18)} style={styles.option_text}>
-                          Option B
-                        </StyledText>
-                        <VoteButton content="B" onPress={() => handlePress("optionB")} />
-                      </View>
-
-                    </View>
-
-                    <SkipButton onPress={handleSkip} style={styles.skip_button} />
-                    <CountDown
-                        finishTime={moment().add({'seconds': getRndInteger(3600/4, 86399)}).toISOString()}
-                        isFinished={()=>console.log('Finished!')}
-                    />
-                  </View>
+              <SkipButton onPress={handleSkip} style={styles.skip_button} />
+              <CountDown
+                finishTime={moment()
+                  .add({ seconds: getRndInteger(3600 / 4, 86399) })
+                  .toISOString()}
+                isFinished={() => console.log("Finished!")}
+              />
+            </View>
           ) : (
-
-             <View style={styles.loading_container}>
+            <View style={styles.loading_container}>
               <Loader />
             </View>
           )}
@@ -119,60 +157,86 @@ const VoteScreen = ({ roomData, userID, badge, handleNextRoom }) => {
       </View>
     </SafeAreaView>
   ) : (
-      <Loader visible={true}/>
+    <Loader visible={true} />
   )
-};
+}
 
-  { /*<Loader />*/}
+const YourVote = ({ selectedOption }) => {
+  const [height, setHeight] = useState(null)
+
+  useEffect(() => console.log("hegiht", height), [height])
+
+  return (
+    <View
+      onLayout={e => setHeight(e.nativeEvent.layout.height)}
+      style={{
+        backgroundColor: colors.secondary.main,
+        width: "48.5%",
+        left: selectedOption === "optionB" ? "51%" : "1%",
+        top: height ? -height : 0,
+        alignItems: "center",
+        paddingVertical: 8
+      }}
+    >
+      <StyledText
+        size={sizes.medium.fontSize}
+        type={"regular"}
+        style={{ color: colors.general.white }}
+      >
+        YOUR VOTE
+      </StyledText>
+    </View>
+  )
+}
+
+{
+  /*<Loader />*/
+}
 const styles = StyleSheet.create({
-  container:{
+  container: {
     flexDirection: "column",
     width: "100%",
-    height: '100%'
+    height: "100%"
   },
-  photo_container:{
-      width: '100%',
-      alignItems: 'center',
-      justifyContent: 'center',
-      height: '40%',
-    },
-
-  buttons_and_timer_container:{
+  photo_container: {
+    width: "100%",
     alignItems: "center",
-    flexDirection: "column",
+    justifyContent: "center",
+    height: "40%"
   },
-  buttons_container:{
+
+  buttons_and_timer_container: {
+    alignItems: "center",
+    flexDirection: "column"
+  },
+  buttons_container: {
     flexDirection: "row",
     justifyContent: "space-between",
-    width: '80%'
+    width: "80%"
   },
-  title_container:{
-    height:'12%',
-    width:'100%',
-    alignItems:'center',
-    justifyContent: 'center'
-  },
-  loading_container:{
+  title_container: {
+    height: "12%",
+    width: "100%",
     alignItems: "center",
-    justifyContent: 'center',
-    width: "100%" ,
-    height: '100%',
-
+    justifyContent: "center"
   },
-  options_container_text:{
+  loading_container: {
+    alignItems: "center",
+    justifyContent: "center",
+    width: "100%",
+    height: "100%"
+  },
+  options_container_text: {
     alignItems: "center"
   },
-  option_text:{
+  option_text: {
     paddingTop: 5,
     color: colors.text.secondary.main
   },
-  skip_button:{
+  skip_button: {
     marginTop: normalize(8),
-    marginBottom:normalize(8)
+    marginBottom: normalize(8)
   }
-});
-
-
-
+})
 
 export default VoteScreen
