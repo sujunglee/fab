4b16145789a9a5604056bf5481e5f18fb0ae810b
Jidé Anene <dominicanene@gmail.com>
2020-03-08 03:18:58 -0500

    Works for IOS not for android

--- src/db/getVoteData.js
+++ src/db/getVoteData.js
@@ -1,17 +1,11 @@
 import fb from "./init"
 import {getNumberOfVoters} from './Utility'
 
-const db = fb.database();
 
 ///might want to delete and just use getRoomData
-const getVoteData = async ({roomID}) => {
-    var snapshot = await db.ref("rooms/active/" + roomID).once("value")
-    if (!snapshot.val()) {
-        snapshot = await db.ref("rooms/inactive/" + roomID).once("value")
-    }
-    console.log("SNAPSHOT: ", snapshot.val());
+const getVoteData =  (roomData) => {
 
-    const {numInfluencersA,numNormalA,numInfluencersB,numNormalB} = getNumberOfVoters(snapshot.val());
+    const {numInfluencersA,numNormalA,numInfluencersB,numNormalB} = getNumberOfVoters(roomData);
     const scoreA = numNormalA + numInfluencersA;
     const scoreB = numNormalB + numInfluencersB;
     return {
