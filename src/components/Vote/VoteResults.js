88273a9419d83484878692ab2534a756533ea545
Zack <szaslan@gmail.com>
2020-03-10 11:45:17 -0500

    [VoteScreen] Move YOUR VOTE to selected photo

--- src/components/Vote/VoteResults.js
+++ src/components/Vote/VoteResults.js
@@ -1,122 +1,182 @@
-import React, { useState, useRef, useEffect, useLayoutEffect, useContext } from "react"
-import {View, StyleSheet} from "react-native"
+import React, {
+  useState,
+  useRef,
+  useEffect,
+  useLayoutEffect,
+  useContext
+} from "react"
+import { View, StyleSheet } from "react-native"
 import { StyledText } from "../StyledText"
 import { colors, normalize, sizes } from "../../constants/styles"
 
-const VoteResults = (props) => {
+const VoteResults = props => {
   return (
+    <View
+      style={{
+        ...StyleSheet.absoluteFillObject,
+        backgroundColor: "rgba(0, 0, 0, 0.7)",
+        borderRadius: 30
+      }}
+    >
+      {/*Get the container from for title from VoteScreen to align container below - hack */}
+      <View style={styles.title_container} />
+
       <View
         style={{
-          ...StyleSheet.absoluteFillObject,
-          backgroundColor: "rgba(0, 0, 0, 0.7)",
-            borderRadius: 30
+          flexDirection: "row",
+          alignItems: "center",
+          justifyContent: "center",
+          height: props.viewport.height,
+          width: props.viewport.width
         }}
       >
-          {/*Get the container from for title from VoteScreen to align container below - hack */}
-        <View style={styles.title_container}/>
+        {props.scoreA === 0 && props.scoreB === 0 && (
+          <View
+            style={{
+              width: "100%",
+              flexDirection: "column",
+              alignItems: "center",
+              justifyContent: "center"
+            }}
+          >
+            <StyledText
+              type="bold"
+              style={{ color: "white", fontSize: sizes.large.fontSize }}
+            >
+              You've made the first vote.
+            </StyledText>
+            <StyledText
+              style={{ color: "white", fontSize: sizes.medium.fontSize }}
+            >
+              So far you are 100%!
+            </StyledText>
+          </View>
+        )}
 
-        <View style={{
-            flexDirection: "row",
-            alignItems: "center",
-            justifyContent: 'center',
-            height: props.viewport.height,
-            width: props.viewport.width
-        }}>
+        {(props.scoreA !== 0 || props.scoreB !== 0) && (
+          <View
+            style={{
+              width: "50%",
+              height: "100%",
+              alignItems: "center",
+              justifyContent: "center"
+            }}
+          >
+            <VotePercents
+              score="A"
+              scoreA={props.scoreA}
+              scoreB={props.scoreB}
+              imageViewport={props.viewport}
+            />
+          </View>
+        )}
 
-            {(props.scoreA === 0 && props.scoreB === 0) && (
-              <View
-                style={{
-                  width: "100%",
-                  flexDirection: "column",
-                  alignItems: "center",
-                    justifyContent:"center"
-                }}
-              >
-                <StyledText type="bold" style={{ color: "white", fontSize: sizes.large.fontSize }}>
-                  You've made the first vote.
-              </StyledText>
-                <StyledText style={{ color: "white", fontSize: sizes.medium.fontSize }}>
-                  So far you are 100%!
-              </StyledText>
-              </View>
-            )}
-
-
-            {(props.scoreA !== 0 || props.scoreB !== 0) &&
-               <View style={{ width: "50%" , height:"100%",alignItems: 'center', justifyContent: 'center'}}>
-                  <VotePercents
-                    score="A"
-                    scoreA={props.scoreA}
-                    scoreB={props.scoreB}
-                    imageViewport={props.viewport}
-                  />
-                </View>}
-
-           {(props.scoreB !== 0 ||props.scoreA !== 0)   &&
-           <View style={{ width: "50%",height:"100%",alignItems: 'center', justifyContent: 'center' }}>
-              <VotePercents
-                score="B"
-                scoreA={props.scoreA}
-                scoreB={props.scoreB}
-                imageViewport={props.viewport}
-              />
-          </View>}
-
-
+        {(props.scoreB !== 0 || props.scoreA !== 0) && (
+          <View
+            style={{
+              width: "50%",
+              height: "100%",
+              alignItems: "center",
+              justifyContent: "center"
+            }}
+          >
+            <VotePercents
+              score="B"
+              scoreA={props.scoreA}
+              scoreB={props.scoreB}
+              imageViewport={props.viewport}
+            />
+          </View>
+        )}
       </View>
 
-
-
-      {props.selectedOption && <YourVote selectedOption={props.selectedOption} />}
-      </View>
+      {props.selectedOption && (
+        <YourVote selectedOption={props.selectedOption} />
+      )}
+    </View>
   )
-};
+}
 
-const VotePercents = (props) => {
-    const textStyles = {
+const VotePercents = props => {
+  const textStyles = {
     color: "white",
     fontSize: normalize(30)
   }
   const borderStyles = {
     height: normalize(80),
-      width: normalize(80),
-      alignItems: 'center',
-      justifyContent: 'center',
-      paddingLeft:normalize(5)
+    width: normalize(80),
+    alignItems: "center",
+    justifyContent: "center",
+    paddingLeft: normalize(5)
   }
-  var borderColors = (props.scoreA > props.scoreB) ? { borderA: colors.secondary.main, borderB: "transparent" } : { borderB: colors.secondary.main, borderA: "transparent" }
-  if (props.scoreA === props.scoreB){
+  var borderColors =
+    props.scoreA > props.scoreB
+      ? { borderA: colors.secondary.main, borderB: "transparent" }
+      : { borderB: colors.secondary.main, borderA: "transparent" }
+  if (props.scoreA === props.scoreB) {
     borderColors = { borderA: "transparent", borderB: "transparent" }
   }
   return props.score === "A" ? (
-
-      <View style={{...borderStyles,borderColor: borderColors.borderA }}>
-          <View style={{flexDirection:'row', height:normalize(40), width: normalize(60),justifyContent:'center'}}>
-            <StyledText type={'bold'} style={{ ...textStyles,alignSelf:'flex-end' }}>
-              {((props.scoreA / (props.scoreA + props.scoreB)) * 100).toFixed()}
-            </StyledText>
-            <StyledText style={{...textStyles, fontSize:sizes.small.fontSize, alignSelf:'flex-end', marginBottom:normalize(5)}}>%</StyledText>
-          </View>
+    <View style={{ ...borderStyles, borderColor: borderColors.borderA }}>
+      <View
+        style={{
+          flexDirection: "row",
+          height: normalize(40),
+          width: normalize(60),
+          justifyContent: "center"
+        }}
+      >
+        <StyledText
+          type={"bold"}
+          style={{ ...textStyles, alignSelf: "flex-end" }}
+        >
+          {((props.scoreA / (props.scoreA + props.scoreB)) * 100).toFixed()}
+        </StyledText>
+        <StyledText
+          style={{
+            ...textStyles,
+            fontSize: sizes.small.fontSize,
+            alignSelf: "flex-end",
+            marginBottom: normalize(5)
+          }}
+        >
+          %
+        </StyledText>
       </View>
-
+    </View>
   ) : (
-      <View style={{...borderStyles,borderColor: borderColors.borderB }}>
-          <View style={{flexDirection:'row', height:normalize(40), width: normalize(60),justifyContent:'center'}}>
-            <StyledText type={'bold'}  style={{ ...textStyles,alignSelf:'flex-end' }}>
-              {((props.scoreB / (props.scoreA + props.scoreB)) * 100).toFixed()}
-            </StyledText>
-            <StyledText style={{...textStyles, fontSize:sizes.small.fontSize, alignSelf:'flex-end', marginBottom:normalize(5)}}>%</StyledText>
-          </View>
+    <View style={{ ...borderStyles, borderColor: borderColors.borderB }}>
+      <View
+        style={{
+          flexDirection: "row",
+          height: normalize(40),
+          width: normalize(60),
+          justifyContent: "center"
+        }}
+      >
+        <StyledText
+          type={"bold"}
+          style={{ ...textStyles, alignSelf: "flex-end" }}
+        >
+          {((props.scoreB / (props.scoreA + props.scoreB)) * 100).toFixed()}
+        </StyledText>
+        <StyledText
+          style={{
+            ...textStyles,
+            fontSize: sizes.small.fontSize,
+            alignSelf: "flex-end",
+            marginBottom: normalize(5)
+          }}
+        >
+          %
+        </StyledText>
       </View>
-
-    )
-
+    </View>
+  )
 }
 
 const YourVote = ({ selectedOption }) => {
   const [height, setHeight] = useState(null)
-
-  useEffect(() => console.log("hegiht", height), [height])
 
   return (
     <View
@@ -124,6 +184,7 @@ const YourVote = ({ selectedOption }) => {
       style={{
         backgroundColor: colors.secondary.main,
         width: "49%",
+        left: selectedOption === "optionA" ? 0 : "51%",
         top: height ? -height : 0,
         alignItems: "center",
         paddingVertical: 8
@@ -145,9 +206,8 @@ const styles = StyleSheet.create({
     height: "12%",
     width: "95%",
     alignItems: "center",
-    justifyContent: "center",
+    justifyContent: "center"
   }
-});
-
+})
 
 export default VoteResults
--- src/components/Vote/VoteScreen.js
+++ src/components/Vote/VoteScreen.js
@@ -1,11 +1,17 @@
-import React, { useState, useRef, useEffect, useLayoutEffect, useContext } from "react"
+import React, {
+  useState,
+  useRef,
+  useEffect,
+  useLayoutEffect,
+  useContext
+} from "react"
 import { View, Text, Dimensions, StyleSheet } from "react-native"
 import { VoteButton, SkipButton } from "./VoteButton"
 import { SafeAreaView } from "react-native-safe-area-context"
 import { StyledText } from "../StyledText"
 import updateVotes from "../../db/updateVotes"
-import getVoteData from "../../db/getVoteData";
-import getRoomData from "../../db/getRoomData";
+import getVoteData from "../../db/getVoteData"
+import getRoomData from "../../db/getRoomData"
 import closeRoom from "../../db/closeRoom"
 import { colors, normalize, sizes } from "../../constants/styles"
 import CountDown from "../countdown/CountDown"
@@ -14,12 +20,12 @@ import { VotingChart } from "../VotingChart"
 import { RoomImages } from "../RoomImages"
 import { RoomTitle } from "../RoomTitle"
 import Loader from "../FancyLoader/FancyLoader"
-import { AppContext } from "../../context/AppContext";
-import { Surface } from "react-native-paper";
-import { VoteContext } from "./VoteContext/VoteContext";
-import VoteResults from "./VoteResults";
+import { AppContext } from "../../context/AppContext"
+import { Surface } from "react-native-paper"
+import { VoteContext } from "./VoteContext/VoteContext"
+import VoteResults from "./VoteResults"
 import fb from "../../db/init"
-const db = fb.database();
+const db = fb.database()
 
 /*
 function getRndInteger(min, max) {
@@ -30,27 +36,29 @@ function getRndInteger(min, max) {
 const VoteScreen = ({ roomInfo }) => {
   const [voteState, setVoteState] = useState({})
   const deviceWidth = Dimensions.get("window").width
-  const [areImagesLoaded, setAreImagesLoaded] = useState(false);
+  const [areImagesLoaded, setAreImagesLoaded] = useState(false)
   const [imageViewport, setImageViewport] = useState({})
-  const { user, userID, isLoggedIn } = useContext(AppContext);
-  const { currentRoom, setCurrentRoom, swiper } = useContext(VoteContext);
-  const [hasFinished, setHasFinished] = useState(false);
-  const [roomData, setRoomData] = useState(null);
+  const { user, userID, isLoggedIn } = useContext(AppContext)
+  const { currentRoom, setCurrentRoom, swiper } = useContext(VoteContext)
+  const [hasFinished, setHasFinished] = useState(false)
+  const [roomData, setRoomData] = useState(null)
 
   // continuously get data
   useEffect(() => {
+    const handleData = snapshot => {
+      snapshot.val() && setRoomData(snapshot.val())
+    }
 
-    const handleData = snapshot =>{
-        snapshot.val() && setRoomData(snapshot.val())
-    };
+    db.ref("rooms/active/")
+      .child(roomInfo.id)
+      .on("value", handleData, error => alert(`[VoteScreen]${error}`))
 
-    db.ref('rooms/active/')
+    return () =>
+      db
+        .ref("rooms/active/")
         .child(roomInfo.id)
-        .on('value', handleData, (error) => alert(`[VoteScreen]${error}`));
-
-    return ()=> db.ref('rooms/active/').child(roomInfo.id).off('value',handleData)
-  }, []);
-
+        .off("value", handleData)
+  }, [])
 
   const handlePress = async selection => {
     const roomID = roomInfo.id
@@ -72,38 +80,47 @@ const VoteScreen = ({ roomInfo }) => {
 
     const delay = 3000
     setTimeout(() => {
-      swiper.current.swipeTop();
+      swiper.current.swipeTop()
       setVoteState({})
       setAreImagesLoaded(false)
     }, delay)
   }
 
   const handleSkip = () => {
-    setAreImagesLoaded(false);
-    swiper.current.swipeTop();
-  };
+    setAreImagesLoaded(false)
+    swiper.current.swipeTop()
+  }
 
   const imageLoadCallback = () => {
     setAreImagesLoaded(true)
-  };
+  }
 
   const isFinishedCallback = () => {
-    const roomID = roomInfo.id;
-    closeRoom({ roomID}).then(()=>{
-          getRoomData({roomID}).then((roomData)=> {
-            const voteResults = getVoteData(roomData);
-            setVoteState({
-              ...voteState,
-              selectedOption: null,
-              voteResults
-              });
-            })
-    });
-  };
+    const roomID = roomInfo.id
+    closeRoom({ roomID }).then(() => {
+      getRoomData({ roomID }).then(roomData => {
+        const voteResults = getVoteData(roomData)
+        setVoteState({
+          ...voteState,
+          selectedOption: null,
+          voteResults
+        })
+      })
+    })
+  }
 
   return roomData && isLoggedIn ? (
-    <Surface style={{ height: "96%", width:normalize(310), alignSelf:'center',borderRadius: 30, borderWidth: .5, borderColor: 'rgba(0,0,0,.1)' }}>
-      <View >
+    <Surface
+      style={{
+        height: "96%",
+        width: normalize(310),
+        alignSelf: "center",
+        borderRadius: 30,
+        borderWidth: 0.5,
+        borderColor: "rgba(0,0,0,.1)"
+      }}
+    >
+      <View>
         <View style={styles.container}>
           {/*
         black screen overlay and vote results over images
@@ -125,21 +142,33 @@ const VoteScreen = ({ roomInfo }) => {
           </View>
 
           {voteState.voteResults && (
-            <VoteResults scoreA={voteState.voteResults.scoreA}
-                         scoreB={voteState.voteResults.scoreB} 
-                         viewport={imageViewport}
-                         selectedOption={voteState.selectedOption}
+            <VoteResults
+              scoreA={voteState.voteResults.scoreA}
+              scoreB={voteState.voteResults.scoreB}
+              viewport={imageViewport}
+              selectedOption={voteState.selectedOption}
             />
           )}
 
-          <View style={{ width: "100%", height: "48%", flexDirection:'row', alignItems:'center',justifyContent:'center' }}>
+          <View
+            style={{
+              width: "100%",
+              height: "48%",
+              flexDirection: "row",
+              alignItems: "center",
+              justifyContent: "center"
+            }}
+          >
             {voteState.voteResults ? (
               <></>
             ) : areImagesLoaded ? (
               <View style={styles.buttons_and_timer_container}>
                 <View style={styles.buttons_container}>
                   <View style={styles.options_container_text}>
-                    <View size={normalize(18)} style={styles.option_text}></View>
+                    <View
+                      size={normalize(18)}
+                      style={styles.option_text}
+                    ></View>
                     <VoteButton
                       content="A"
                       onPress={() => handlePress("optionA")}
@@ -147,7 +176,10 @@ const VoteScreen = ({ roomInfo }) => {
                   </View>
 
                   <View style={styles.options_container_text}>
-                    <View size={normalize(18)} style={styles.option_text}></View>
+                    <View
+                      size={normalize(18)}
+                      style={styles.option_text}
+                    ></View>
                     <VoteButton
                       content="B"
                       onPress={() => handlePress("optionB")}
@@ -162,20 +194,20 @@ const VoteScreen = ({ roomInfo }) => {
                 />
               </View>
             ) : (
-                  <View style={styles.loading_container}>
-                    <Loader />
-                  </View>
-                )}
+              <View style={styles.loading_container}>
+                <Loader />
+              </View>
+            )}
           </View>
         </View>
       </View>
     </Surface>
   ) : (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Loader visible={true} />
-      </View>
-    )
-};
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Loader visible={true} />
+    </View>
+  )
+}
 
 {
   /*<Loader />*/
@@ -185,8 +217,8 @@ const styles = StyleSheet.create({
     flexDirection: "column",
     width: "100%",
     height: "100%",
-    alignItems: 'center',
-    justifyContent: 'center'
+    alignItems: "center",
+    justifyContent: "center"
   },
   photo_container: {
     width: "100%",
@@ -199,7 +231,7 @@ const styles = StyleSheet.create({
     alignItems: "center",
     flexDirection: "column",
     height: normalize(220),
-    width: '100%'
+    width: "100%"
   },
   buttons_container: {
     flexDirection: "row",
@@ -210,7 +242,7 @@ const styles = StyleSheet.create({
     height: "12%",
     width: "95%",
     alignItems: "center",
-    justifyContent: "center",
+    justifyContent: "center"
   },
   loading_container: {
     alignItems: "center",
@@ -224,7 +256,7 @@ const styles = StyleSheet.create({
   option_text: {
     paddingTop: normalize(5),
     color: colors.text.secondary.main,
-    paddingBottom: normalize(20),
+    paddingBottom: normalize(20)
   },
   skip_button: {
     marginTop: normalize(8),
