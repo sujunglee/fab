95a75b4f585b0b14138ae7b6fb314d6e6fe5cfba
Jidé Anene <dominicanene@gmail.com>
2020-03-10 10:59:14 -0500

    match UI for VoteResults

--- src/components/Vote/VoteResults.js
+++ src/components/Vote/VoteResults.js
@@ -12,25 +12,6 @@ const VoteResults = (props) => {
             borderRadius: 30
         }}
       >
-      {props.scoreA === 0 &&
-        props.scoreB === 0 && (
-          <View
-            style={{
-              width: "100%",
-              flexDirection: "column",
-              alignItems: "center",
-              bottom: props.viewport.height / 2 + 10
-            }}
-          >
-            <StyledText type="bold" style={{ color: "white" }}>
-              You've made the first vote.
-          </StyledText>
-            <StyledText style={{ color: "white" }}>
-              So far you are 100%!
-          </StyledText>
-          </View>
-        )}
-
           {/*Get the container from for title from VoteScreen to align container below - hack */}
         <View style={styles.title_container}/>
 
@@ -43,23 +24,50 @@ const VoteResults = (props) => {
             width: props.viewport.width
         }}>
 
-        <View style={{ width: "50%" }}>
-          <VotePercents
-            score="A"
-            scoreA={props.scoreA}
-            scoreB={props.scoreB}
-            imageViewport={props.viewport}
-          />
-        </View>
-        <View style={{ width: "50%" }}>
-          <VotePercents
-            score="B"
-            scoreA={props.scoreA}
-            scoreB={props.scoreB}
-            imageViewport={props.viewport}
-          />
-        </View>
+            {(props.scoreA === 0 && props.scoreB === 0) && (
+              <View
+                style={{
+                  width: "100%",
+                  flexDirection: "column",
+                  alignItems: "center",
+                    justifyContent:"center"
+                }}
+              >
+                <StyledText type="bold" style={{ color: "white", fontSize: sizes.large.fontSize }}>
+                  You've made the first vote.
+              </StyledText>
+                <StyledText style={{ color: "white", fontSize: sizes.medium.fontSize }}>
+                  So far you are 100%!
+              </StyledText>
+              </View>
+            )}
+
+
+            {(props.scoreA !== 0 || props.scoreB !== 0) &&
+               <View style={{ width: "50%" , height:"100%",alignItems: 'center', justifyContent: 'center'}}>
+                  <VotePercents
+                    score="A"
+                    scoreA={props.scoreA}
+                    scoreB={props.scoreB}
+                    imageViewport={props.viewport}
+                  />
+                </View>}
+
+           {(props.scoreB !== 0 ||props.scoreA !== 0)   &&
+           <View style={{ width: "50%",height:"100%",alignItems: 'center', justifyContent: 'center' }}>
+              <VotePercents
+                score="B"
+                scoreA={props.scoreA}
+                scoreB={props.scoreB}
+                imageViewport={props.viewport}
+              />
+          </View>}
+
+
       </View>
+
+
+
       {props.selectedOption && <YourVote selectedOption={props.selectedOption} />}
       </View>
   )
@@ -68,29 +76,42 @@ const VoteResults = (props) => {
 const VotePercents = (props) => {
     const textStyles = {
     color: "white",
-    fontSize: 48
+    fontSize: normalize(30)
   }
   const borderStyles = {
-    borderWidth: 10,
-    textAlign: "center",
-    height: 70,
-    padding: 5,
-    marginRight: 10,
-    marginLeft: 10
+    height: normalize(80),
+      width: normalize(80),
+      alignItems: 'center',
+      justifyContent: 'center',
+      paddingLeft:normalize(5)
   }
   var borderColors = (props.scoreA > props.scoreB) ? { borderA: colors.secondary.main, borderB: "transparent" } : { borderB: colors.secondary.main, borderA: "transparent" }
-  if (props.scoreA == props.scoreB){
+  if (props.scoreA === props.scoreB){
     borderColors = { borderA: "transparent", borderB: "transparent" }
   }
   return props.score === "A" ? (
-    <StyledText style={{ ...textStyles, ...borderStyles, borderColor: borderColors.borderA }}>
-      {((props.scoreA / (props.scoreA + props.scoreB)) * 100).toFixed()}%
-    </StyledText>
+
+      <View style={{...borderStyles,borderColor: borderColors.borderA }}>
+          <View style={{flexDirection:'row', height:normalize(40), width: normalize(60),justifyContent:'center'}}>
+            <StyledText type={'bold'} style={{ ...textStyles,alignSelf:'flex-end' }}>
+              {((props.scoreA / (props.scoreA + props.scoreB)) * 100).toFixed()}
+            </StyledText>
+            <StyledText style={{...textStyles, fontSize:sizes.small.fontSize, alignSelf:'flex-end', marginBottom:normalize(5)}}>%</StyledText>
+          </View>
+      </View>
+
   ) : (
-      <StyledText style={{ ...textStyles, ...borderStyles, borderColor: borderColors.borderB}}>
-        {((props.scoreB / (props.scoreA + props.scoreB)) * 100).toFixed()}%
-        </StyledText>
+      <View style={{...borderStyles,borderColor: borderColors.borderB }}>
+          <View style={{flexDirection:'row', height:normalize(40), width: normalize(60),justifyContent:'center'}}>
+            <StyledText type={'bold'}  style={{ ...textStyles,alignSelf:'flex-end' }}>
+              {((props.scoreB / (props.scoreA + props.scoreB)) * 100).toFixed()}
+            </StyledText>
+            <StyledText style={{...textStyles, fontSize:sizes.small.fontSize, alignSelf:'flex-end', marginBottom:normalize(5)}}>%</StyledText>
+          </View>
+      </View>
+
     )
+
 }
 
 const YourVote = ({ selectedOption }) => {
